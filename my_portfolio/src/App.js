7822c3874f70d2d70d4dc17eb870edc7a3d137d1
moos <herget06@gmail.com>
2022-01-07 11:04:57 -0500

    pdf test

--- my_portfolio/src/App.js
+++ my_portfolio/src/App.js
@@ -4,6 +4,15 @@ import Icon, { CssThree, Github, Gmail, Html5, Javascript, Linkedin, Microsoftsq
 import { yellow } from '@material-ui/core/colors';
 import React from 'react';
 import {Link} from 'react-router-dom';
+// import { PDFDownloadLink, Document, Page } from '@react-pdf/renderer';
+
+// const MyDoc = () => (
+//   <Document>
+//     <Page>
+      
+//     </Page>
+//   </Document>
+// )
 
 function App() {
   return (
@@ -18,7 +27,7 @@ function App() {
         <a href='#skills-section'>Skills</a>
         <a href='#projects-section'>Projects</a>
         <a href='#contact-section'>Contact</a>
-        <a href="./StephenHerget_Resume_2021.09.17x.pdf" download rel='noopener' target="_blank">Resume</a>
+        <a href="./StephenHerget_Resume_2021.09.17x.pdf" download alt=' ' rel='noopener' target="_blank">Resume</a>
       </div>
 
         <div className='middle-header'></div>
