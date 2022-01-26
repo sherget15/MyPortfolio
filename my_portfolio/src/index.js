5fb63f6bf3e5ab50d79f68ecbfbda7f5de916ecb
moos <herget06@gmail.com>
2022-01-26 17:23:41 -0500

    test

--- my_portfolio/src/index.js
+++ my_portfolio/src/index.js
@@ -2,7 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import './index.html';
 import reportWebVitals from './reportWebVitals';
 import {BrowserRouter as Router} from 'react-router-dom';
 
